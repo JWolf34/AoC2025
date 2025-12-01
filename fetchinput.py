5564adf87fb62f0af736fbe95f7d86b273e67e1b
John Wolf <johnmwolf34@gmail.com>
2025-11-30 22:56:05 -0600

    update fetch_input to work for this repo

---
+++ fetchinput.py
@@ -0,0 +1,32 @@
+import datetime
+import requests
+import os
+
+def fetchInput(day, year):
+    
+    print(f"Fetching input for Day {day} of year {year}...")
+    session_is = getSession()
+    cookies_are = dict(session=session_is)
+    headers = {"User-Agent": "johnmwolf34@gmail.com"}
+    r = requests.get(f"https://adventofcode.com/{year}/day/{day}/input",headers=headers,cookies=cookies_are)
+
+    try:
+        input = open(f"day{day}/input.txt", 'a')
+        input.write(r.text)
+        input.close()
+        print("Fetched! Happy coding :)")
+    except FileNotFoundError as e:
+        print("Error occurred when fetching today's input. Is it not the Advent of Code?")
+        print(e)
+
+
+def getSession():
+    return os.getenv("SESSION")
+
+if __name__ == "__main__":
+    DAY = 3
+    YEAR = 2024
+    fetchInput(day= DAY, year= YEAR)
+
+
+    
